53ba7d9ffd95c140b03a7a138379658270401f51
Mervens Val <56974470+MervensVal@users.noreply.github.com>
2024-05-30 12:45:09 -0400

    Update main added try except.py

--- Project/main.py
+++ Project/main.py
@@ -1,4 +1,8 @@
 import db
-db.Create_Reports_Folder()
-db.Create_Tables()
-db.Extract_Products()
+
+try:
+  db.Create_Reports_Folder()
+  db.Create_Tables()
+  db.Extract_Products()
+except Exception as e:
+  print("Error occured during program execution.")
