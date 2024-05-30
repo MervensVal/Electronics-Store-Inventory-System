0a2ce6e42eb10e469dcac62f4f69166a01f1b5b7
Mervens Val <56974470+MervensVal@users.noreply.github.com>
2024-05-30 13:13:21 -0400

    Update main.py added reports

--- Project/main.py
+++ Project/main.py
@@ -4,5 +4,7 @@ try:
   db.Create_Reports_Folder()
   db.Create_Tables()
   db.Extract_Products()
+  db.Generate_Report1()
+  db.Generate_Report2()
 except Exception as e:
   print("Error occured during program execution.")
